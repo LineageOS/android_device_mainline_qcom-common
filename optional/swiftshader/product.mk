d9e67bcc8aa58b01d56d16241e84b7190b150aaa
Yumi Yukimura <me.cafebabe@gmail.com>
2025-04-07 03:53:11 +0800

    mainline/qcom-common: Fix Swiftshader graphics not working with minigbm gralloc
    * Taken from device/linaro/dragonboard
    
    Change-Id: I2acf14370ddcfbcd086ddc7394cfe89bf7cce45a

---
+++ optional/swiftshader/product.mk
@@ -0,0 +1,13 @@
+#
+# SPDX-FileCopyrightText: The LineageOS Project
+# SPDX-License-Identifier: Apache-2.0
+#
+
+ifeq ($(TARGET_GRAPHICS),swiftshader)
+
+ifeq ($(TARGET_GRAPHICS_ALLOCATOR_HAL),minigbm)
+PRODUCT_VENDOR_PROPERTIES += \
+    vendor.minigbm.debug=nocompression
+endif # TARGET_GRAPHICS_ALLOCATOR_HAL
+
+endif # TARGET_GRAPHICS
