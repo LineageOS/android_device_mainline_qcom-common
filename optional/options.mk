cf45e7a59eaa4a76977edc0969d8d2d7dd7b2eac
Yumi Yukimura <me.cafebabe@gmail.com>
2025-04-12 20:49:04 +0800

    mainline/qcom-common: options: Use gbm gralloc if using Mesa for msm8937 and msm8953
    * That gralloc implementation works fine as-is
    * For minigbm we need to make the function `should_avoid_ubwc()` on
      msm.c always return true to get display working with Mesa.
      Not needed for Swiftshader.
    
    Change-Id: I635947b70fe152e13c220882380105bedcc3016e

--- optional/options.mk
+++ optional/options.mk
@@ -18,3 +18,13 @@ endif
 
 # Inherit from mainline/common
 include device/mainline/common/optional/options.mk
+
+##### Do not add statements below the inherit in above unless necessary #####
+
+# Graphics HALs
+## Use gbm gralloc for SoCs which has no display with minigbm when using Mesa
+ifneq ($(filter msm8937 msm8953,$(TARGET_QCOM_SOC_FAMILY)),)
+    ifeq ($(TARGET_GRAPHICS),mesa)
+        TARGET_GRAPHICS_ALLOCATOR_HAL := gbm
+    endif
+endif
