f41cb95860dac5f2b275c29d8f74abbc8c4ffa0e
Yumi Yukimura <me.cafebabe@gmail.com>
2025-05-19 18:14:17 +0800

    mainline/qcom-common: init: Make all the modem services optional
    Change-Id: I8add156435c5132ce793bc2705055b23d1080315

--- mainline_qcom-common.mk
+++ mainline_qcom-common.mk
@@ -23,14 +23,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     init.mainline.qcom.sh
 
-# Modem
-PRODUCT_PACKAGES += \
-    pd-mapper \
-    qrtr-cfg \
-    qrtr-ns \
-    rmtfs \
-    tqftpserv
-
 # Mountpoint
 PRODUCT_PACKAGES += \
     vendor_firmware_gen_mountpoint \
