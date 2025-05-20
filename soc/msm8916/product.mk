e6ee1ab623b33d9c367feb093e2f0dd64b8927de
Yumi Yukimura <me.cafebabe@gmail.com>
2025-05-20 21:11:38 +0800

    mainline/qcom-common: Add initial support for MSM8916 family
    Change-Id: If04881a67fb4436ba656001295fe517286209fd0

---
+++ soc/msm8916/product.mk
@@ -0,0 +1,21 @@
+#
+# SPDX-FileCopyrightText: The LineageOS Project
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Firmware
+LOCAL_FIRMWARE_GPU_A3XX := \
+    firmware_qcom_a300_pfp.fw \
+    firmware_qcom_a300_pm4.fw
+LOCAL_FIRMWARE_GPU_A4XX := \
+    firmware_qcom_a420_pfp.fw \
+    firmware_qcom_a420_pm4.fw
+ifneq ($(filter msm8916 msm8916v2,$(TARGET_QCOM_SOC)),)
+PRODUCT_PACKAGES += $(LOCAL_FIRMWARE_GPU_A3XX)
+else ifneq ($(filter msm8929 msm8936 msm8939 msm8939v2,$(TARGET_QCOM_SOC)),)
+PRODUCT_PACKAGES += $(LOCAL_FIRMWARE_GPU_A4XX)
+else
+PRODUCT_PACKAGES += \
+    $(LOCAL_FIRMWARE_GPU_A3XX) \
+    $(LOCAL_FIRMWARE_GPU_A4XX)
+endif
