e7286317ab6f8915fd7d8c27844458b07afec853
Yumi Yukimura <me.cafebabe@gmail.com>
2025-06-03 02:20:32 +0800

    mainline/qcom-common: soc: msm8916: Copy audio/ from msm8937
    Change-Id: I514eb5c11cc2f7017101037fea8d7ce3ac825e3b

--- soc/msm8916/product.mk
+++ soc/msm8916/product.mk
@@ -3,6 +3,10 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# Audio
+PRODUCT_COPY_FILES += \
+    $(MAINLINE_QCOM_COMMON_SOC_PATH)/audio/primary_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/primary_audio_policy_configuration.xml
+
 # Firmware
 PRODUCT_PACKAGES += \
     linux_firmware_qcom-a300 \
