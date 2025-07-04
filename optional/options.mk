526e72b79a7c3020bfa5e0d0a86138c5b800c18f
Yumi Yukimura <me.cafebabe@gmail.com>
2025-07-05 00:57:19 +0800

    mainline/qcom-common: Simplify handling of `TARGET_QCOM_SOC`
    Change-Id: I9f5b1096c30b57570f369141bbaeed9a442887dc

--- optional/options.mk
+++ optional/options.mk
@@ -8,13 +8,13 @@ ifeq ($(TARGET_QCOM_SOC_FAMILY),)
     ifeq ($(TARGET_QCOM_SOC),)
         $(error Please define either TARGET_QCOM_SOC or TARGET_QCOM_SOC_FAMILY)
     else
-        ifneq ($(filter msm8916 msm8916v2 msm8929 msm8936 msm8939 msm8939v2 msm8939v3,$(TARGET_QCOM_SOC)),)
+        ifneq ($(filter msm8916% msm8929 msm8936 msm8939%,$(TARGET_QCOM_SOC)),)
             TARGET_QCOM_SOC_FAMILY := msm8916
         else ifneq ($(filter msm8917 msm8920 msm8937 msm8940 qcm2150 qm215 sdm429 sdm439,$(TARGET_QCOM_SOC)),)
             TARGET_QCOM_SOC_FAMILY := msm8937
-        else ifneq ($(filter msm8953 msm8953pro sdm450 sdm632,$(TARGET_QCOM_SOC)),)
+        else ifneq ($(filter msm8953% sdm450 sdm632,$(TARGET_QCOM_SOC)),)
             TARGET_QCOM_SOC_FAMILY := msm8953
-        else ifneq ($(filter sm7150-%,$(TARGET_QCOM_SOC)),)
+        else ifneq ($(filter sm7150%,$(TARGET_QCOM_SOC)),)
             TARGET_QCOM_SOC_FAMILY := sm7150
         else
             $(error Please add the SoC to this section)
--- soc/msm8916/product.mk
+++ soc/msm8916/product.mk
@@ -13,9 +13,9 @@ LOCAL_FIRMWARE_GPU_A3XX := \
 LOCAL_FIRMWARE_GPU_A4XX := \
     firmware_qcom_a420_pfp.fw \
     firmware_qcom_a420_pm4.fw
-ifneq ($(filter msm8916 msm8916v2,$(TARGET_QCOM_SOC)),)
+ifneq ($(filter msm8916%,$(TARGET_QCOM_SOC)),)
 PRODUCT_PACKAGES += $(LOCAL_FIRMWARE_GPU_A3XX)
-else ifneq ($(filter msm8929 msm8936 msm8939 msm8939v2 msm8939v3,$(TARGET_QCOM_SOC)),)
+else ifneq ($(filter msm8929 msm8936 msm8939%,$(TARGET_QCOM_SOC)),)
 PRODUCT_PACKAGES += $(LOCAL_FIRMWARE_GPU_A4XX)
 else
 PRODUCT_PACKAGES += \
