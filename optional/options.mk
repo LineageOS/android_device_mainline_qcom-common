a99183e79b6c9029e5ede1e564c9803b42857ad7
Yumi Yukimura <me.cafebabe@gmail.com>
2025-07-28 06:19:28 +0800

    mainline/qcom-common: options.mk: Now gbm gralloc is default in mainline/common
    Change-Id: I475281342a7f22833f6470967ba87a96acc343e2

--- optional/options.mk
+++ optional/options.mk
@@ -32,15 +32,14 @@ ifeq ($(AB_OTA_UPDATER),true)
 TARGET_BOOT_HAL ?= qcom-caf-aidl
 endif
 
+# Graphics HALs
+## Avoid using minigbm for incompatible SoCs by default
+## (most of the incompatible SoCs just needs should_avoid_ubwc quirk)
+ifeq ($(filter msm8916 msm8937 msm8953,$(TARGET_QCOM_SOC_FAMILY)),)
+    TARGET_GRAPHICS_ALLOCATOR_HAL ?= minigbm
+endif
+
 # Inherit from mainline/common
 include device/mainline/common/optional/options.mk
 
 ##### Do not add statements below the inherit in above unless necessary #####
-
-# Graphics HALs
-## Use gbm gralloc for SoCs which has no display with minigbm when using Mesa
-ifneq ($(filter msm8916 msm8937 msm8953,$(TARGET_QCOM_SOC_FAMILY)),)
-    ifeq ($(TARGET_GRAPHICS),mesa)
-        TARGET_GRAPHICS_ALLOCATOR_HAL := gbm
-    endif
-endif
