6ddcf2c834009e0f4ed5815745b937cebc44c774
Yumi Yukimura <me.cafebabe@gmail.com>
2025-07-28 05:28:27 +0800

    mainline/qcom-common: soc: msm8953: Set OpenGL ES version
    Change-Id: I26b0f97e4e0f076d5d6a235c8f5be2b8103dec08

--- soc/msm8953/product.mk
+++ soc/msm8953/product.mk
@@ -3,6 +3,12 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# Graphics (Mesa)
+ifeq ($(TARGET_GRAPHICS),mesa)
+PRODUCT_VENDOR_PROPERTIES += \
+    ro.opengles.version=196609
+endif
+
 # Firmware
 PRODUCT_PACKAGES += \
     firmware_qcom_a530_pfp.fw \
