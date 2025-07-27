2f0110c4d837550a85e023a01c000d29f86f18c2
Yumi Yukimura <me.cafebabe@gmail.com>
2025-07-28 03:12:52 +0800

    Revert "mainline/qcom-common: options: Use Swiftshader for known SoCs with Adreno 505/506"
    Mesa works fine on Adreno 505/506 now
    
    This reverts commit 8c05565bc7a6314a773fc7e5328ad1c16bb32cd4.
    
    Change-Id: I8d7a18b76fee2caf16ffbab658492c342e52b077

--- optional/options.mk
+++ optional/options.mk
@@ -32,14 +32,6 @@ ifeq ($(AB_OTA_UPDATER),true)
 TARGET_BOOT_HAL ?= qcom-caf-aidl
 endif
 
-# Graphics
-## Use Swiftshader for SoCs where Mesa doesn't work properly (Adreno A5xx mostly)
-ifneq ($(filter msm8953,$(TARGET_QCOM_SOC_FAMILY)),)
-    TARGET_GRAPHICS ?= swiftshader
-else ifneq ($(filter msm8937 msm8940 sdm439,$(TARGET_QCOM_SOC)),)
-    TARGET_GRAPHICS ?= swiftshader
-endif
-
 # Inherit from mainline/common
 include device/mainline/common/optional/options.mk
 
