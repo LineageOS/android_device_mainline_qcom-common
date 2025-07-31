d97978a6c19e9b1376d793744a30c5ce95431e8b
Yumi Yukimura <me.cafebabe@gmail.com>
2025-08-01 02:18:09 +0800

    mainline/qcom-common: media: Import media_profiles.xml from dragonboard
    * Of course we have to split it into per-SoC later, but let's
      at least have one for now.
    
    Co-authored-by: John Stultz <john.stultz@linaro.org>
    Change-Id: Ifff695789e01740f8e35126f4bf9fbc7d4b8b6ea

--- mainline_qcom-common.mk
+++ mainline_qcom-common.mk
@@ -28,6 +28,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     init.mainline.qcom.sh
 
+# Media
+PRODUCT_COPY_FILES += \
+    $(MAINLINE_QCOM_COMMON_PATH)/media/media_profiles.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles_V1_0.xml
+
 # Mountpoint
 PRODUCT_PACKAGES += \
     mainline_qcom-common_vendor_dsp_mountpoint \
