9a208318032b3d748b31b324ca4de78945c1a041
Aaron Kling <webgeek1234@gmail.com>
2025-11-06 13:25:25 -0600

    mainline/qcom-common: soc: Add support for sm8550
    Copied from sm7150, adapting nodes paths for sm8550
    
    Change-Id: Ib0cad69954c54fe7d49e8785fde1cc994c74611c

---
+++ soc/sm8550/product.mk
@@ -0,0 +1,45 @@
+#
+# SPDX-FileCopyrightText: The LineageOS Project
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Audio
+PRODUCT_COPY_FILES += \
+    $(MAINLINE_QCOM_COMMON_SOC_PATH)/audio/primary_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/primary_audio_policy_configuration.xml
+
+# DSP
+PRODUCT_PACKAGES += \
+    hexagonrpcd_adsp_rootpd_phony \
+    hexagonrpcd_adsp_sensorspd_phony
+
+# Firmware
+PRODUCT_PACKAGES += \
+    linux_firmware_ath12k-wcn7850
+
+# Graphics (Mesa)
+ifeq ($(TARGET_GRAPHICS),mesa)
+PRODUCT_VENDOR_PROPERTIES += \
+    ro.opengles.version=196610
+endif
+
+# Init
+PRODUCT_PACKAGES += \
+    init.mainline.qcom.sm8550.rc
+
+ifneq ($(filter sm8550,$(TARGET_QCOM_SOC)),)
+# Modem
+PRODUCT_PACKAGES += \
+    rmtfs \
+    rmtfs.rc \
+    tqftpserv \
+    tqftpserv.rc
+
+PRODUCT_VENDOR_PROPERTIES += \
+    ro.vendor.qcom.soc.enable_modem_services=1
+endif
+
+# QRTR
+PRODUCT_PACKAGES += \
+    qrtr-cfg \
+    qrtr-ns \
+    qrtr-ns.rc
