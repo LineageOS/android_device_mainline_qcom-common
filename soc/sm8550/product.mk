a86fe5c9e75daf80188b8fa61fd2549004f7bf4c
Aaron Kling <webgeek1234@gmail.com>
2025-11-06 13:25:25 -0600

    mainline/qcom-common: soc: Add support for qcs8550
    Copied from sm7150, adapting nodes paths for sm8550
    
    This only supports qcs8550, there is no modem support for sm8550.
    
    Change-Id: Ib0cad69954c54fe7d49e8785fde1cc994c74611c

---
+++ soc/sm8550/product.mk
@@ -0,0 +1,27 @@
+#
+# SPDX-FileCopyrightText: The LineageOS Project
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Audio
+PRODUCT_COPY_FILES += \
+    $(MAINLINE_QCOM_COMMON_SOC_PATH)/audio/primary_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/primary_audio_policy_configuration.xml
+
+# DSP
+PRODUCT_PACKAGES += \
+    hexagonrpcd_adsp_rootpd_phony \
+    hexagonrpcd_adsp_sensorspd_phony
+
+# Firmware
+PRODUCT_PACKAGES += \
+    linux_firmware_ath12k-wcn7850
+
+# Graphics (Mesa)
+ifeq ($(TARGET_GRAPHICS),mesa)
+PRODUCT_VENDOR_PROPERTIES += \
+    ro.opengles.version=196610
+endif
+
+# Init
+PRODUCT_PACKAGES += \
+    init.mainline.qcom.sm8550.rc
