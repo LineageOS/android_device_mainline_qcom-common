b5f50b092808c2a704927a7b0c0ab7700f84b7a0
Yumi Yukimura <me.cafebabe@gmail.com>
2025-12-05 19:29:07 +0800

    mainline/qcom-common: Add support for upstream minigbm & default to it
    * That minigbm repo contains our additions
      which are mainly required for legacy SoCs.
    * It benefits everyone else too, as it's much closer to upstream
    
    Change-Id: Id147b3232c03afe954e6b79f7973b8204f4495b6

--- optional/swiftshader/product.mk
+++ optional/swiftshader/product.mk
@@ -8,6 +8,9 @@ ifeq ($(TARGET_GRAPHICS),swiftshader)
 ifeq ($(TARGET_GRAPHICS_ALLOCATOR_HAL),minigbm)
 PRODUCT_VENDOR_PROPERTIES += \
     vendor.minigbm.debug=nocompression
+else ifeq ($(TARGET_GRAPHICS_ALLOCATOR_HAL),minigbm-upstream)
+PRODUCT_VENDOR_PROPERTIES += \
+    vendor.minigbm.debug=nocompression
 endif # TARGET_GRAPHICS_ALLOCATOR_HAL
 
 endif # TARGET_GRAPHICS
