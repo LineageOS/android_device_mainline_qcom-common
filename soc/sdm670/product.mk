ab0ecb17cfe9d68ba2c8d5de0c3f056d07f23499
Yumi Yukimura <me.cafebabe@gmail.com>
2026-02-26 23:54:56 +0800

    mainline/qcom-common: soc: Add initial support for SDM670 family
    Change-Id: Ia0a41cb17abc2662adcdb804fdf5a4d6012024f1

---
+++ soc/sdm670/product.mk
@@ -0,0 +1,12 @@
+#
+# SPDX-FileCopyrightText: The LineageOS Project
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Firmware
+PRODUCT_PACKAGES += \
+    linux_firmware_qcom-a630
+
+# Init
+PRODUCT_PACKAGES += \
+    init.mainline.qcom.sdm670.rc
