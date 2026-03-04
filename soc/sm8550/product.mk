a78e396481404aa0a1aeb0f859bb8aaf5ffc3989
Aaron Kling <webgeek1234@gmail.com>
2026-03-03 23:05:31 -0600

    mainline/qcom-common: soc: sm8550: Declare bluetooth le support
    Copy bluetooth le permission if bluetooth is enabled
    
    Change-Id: Ie0f523cdd4248252bbe834bf2d6f2928c254a9bb

--- soc/sm8550/product.mk
+++ soc/sm8550/product.mk
@@ -6,6 +6,12 @@
 # Audio
 $(call soong_config_set,mainline_qcom_common_soc,primary_audio_policy_configuration_variant,sm8550)
 
+# Bluetooth
+ifneq ($(TARGET_BLUETOOTH_HAL),)
+PRODUCT_PACKAGES += \
+    android.hardware.bluetooth_le.prebuilt.xml
+endif
+
 # DSP
 PRODUCT_PACKAGES += \
     hexagonrpcd_adsp_rootpd_phony
