97e7465afb599a3f9ff7bff8578059f7b59d264a
Aaron Kling <webgeek1234@gmail.com>
2026-03-05 17:02:48 -0600

    mainline/qcom-common: soc: sm8550: Declare low latency audio support
    Change-Id: I18e0a12311ef7b331a55e1446bd9e3913ceaa16d

--- soc/sm8550/product.mk
+++ soc/sm8550/product.mk
@@ -9,6 +9,10 @@ ifeq ($(TARGET_AUDIO_HAL),baylibre)
 PRODUCT_VENDOR_PROPERTIES += \
     ro.vendor.audio.primary.latency_ms=40
 endif
+ifneq ($(TARGET_AUDIO_HAL),)
+PRODUCT_PACKAGES += \
+    android.hardware.audio.low_latency.prebuilt.xml
+endif
 
 # Bluetooth
 ifneq ($(TARGET_BLUETOOTH_HAL),)
