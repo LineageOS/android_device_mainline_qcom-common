60631b43efbc2016e0d44228b9081e775ed4e383
Aaron Kling <webgeek1234@gmail.com>
2026-03-21 00:28:32 -0500

    mainline/qcom-common: soc: sm8550: Support v4l2 c2 hal
    Change-Id: Ia8044944b0f27cc11198f0e9932a6649e269300f

--- soc/sm8550/product.mk
+++ soc/sm8550/product.mk
@@ -38,6 +38,21 @@ $(call soong_config_set_string_list,minigbm_upstream,cflags,-DSC_7280)
 PRODUCT_PACKAGES += \
     init.mainline.qcom.sm8550.rc
 
+# Media
+ifeq ($(TARGET_MEDIA_C2_HAL),v4l2_codec2)
+PRODUCT_COPY_FILES += \
+    $(MAINLINE_QCOM_COMMON_SOC_PATH)/media/media_codecs_c2.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_c2.xml
+
+PRODUCT_VENDOR_PROPERTIES += \
+    ro.vendor.v4l2_codec2.decode_concurrent_instances=32 \
+    ro.vendor.v4l2_codec2.encode_concurrent_instances=32
+
+PRODUCT_VENDOR_PROPERTIES += \
+    debug.stagefright.c2-poolmask=0x1f50000
+
+TARGET_V4L2_CODEC2_USE_EXAMPLE_CONFIGURATION := false
+endif # TARGET_MEDIA_C2_HAL
+
 # Thermal
 ifeq ($(TARGET_THERMAL_HAL),linaro-libpm)
 PRODUCT_PACKAGES += \
