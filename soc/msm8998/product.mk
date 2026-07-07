1c616b734b6927afd2791b094f83a84eb12d0014
Yumi Yukimura <me.cafebabe@gmail.com>
2026-07-07 18:36:50 +0800

    mainline/qcom-common: soc: Add initial support for msm8998
    Change-Id: I1f3230594e74d32fe0440afbbac4d48d6e0d4516

---
+++ soc/msm8998/product.mk
@@ -0,0 +1,33 @@
+#
+# SPDX-FileCopyrightText: The LineageOS Project
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Firmware
+## Note: linux-firmware repo does not have a540_gpmu.fw2
+PRODUCT_PACKAGES += \
+    linux_firmware_qcom-a530
+
+# Graphics (Mesa)
+ifeq ($(TARGET_GRAPHICS),mesa)
+PRODUCT_VENDOR_PROPERTIES += \
+    ro.opengles.version=196609
+endif
+
+# Init
+PRODUCT_PACKAGES += \
+    init.mainline.qcom.msm8998.rc
+
+# Modem
+PRODUCT_PACKAGES += \
+    rmtfs \
+    rmtfs.rc \
+    tqftpserv \
+    tqftpserv.rc
+
+PRODUCT_VENDOR_PROPERTIES += \
+    ro.vendor.qcom.soc.enable_modem_services=1
+
+# QRTR
+PRODUCT_PACKAGES += \
+    qrtr-cfg
