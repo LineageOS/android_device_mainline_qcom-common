6584014f3a769cef9c2a60c4d1953a0a7c32559d
Yumi Yukimura <me.cafebabe@gmail.com>
2026-07-07 18:36:50 +0800

    mainline/qcom-common: soc: Add initial support for msm8998
    Change-Id: I1f3230594e74d32fe0440afbbac4d48d6e0d4516

--- optional/options.mk
+++ optional/options.mk
@@ -14,6 +14,8 @@ ifeq ($(TARGET_QCOM_SOC_FAMILY),)
             TARGET_QCOM_SOC_FAMILY := msm8937
         else ifneq ($(filter msm8953% sdm450 sdm632,$(TARGET_QCOM_SOC)),)
             TARGET_QCOM_SOC_FAMILY := msm8953
+        else ifneq ($(filter msm8998,$(TARGET_QCOM_SOC)),)
+            TARGET_QCOM_SOC_FAMILY := msm8998
         else ifneq ($(filter sdm670 sdm710,$(TARGET_QCOM_SOC)),)
             TARGET_QCOM_SOC_FAMILY := sdm670
         else ifneq ($(filter sm7150%,$(TARGET_QCOM_SOC)),)
