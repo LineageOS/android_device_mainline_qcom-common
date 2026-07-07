f84c97265d2c1fc64b4b27a1180855f0300fca5d
Yumi Yukimura <me.cafebabe@gmail.com>
2026-07-07 18:36:50 +0800

    mainline/qcom-common: soc: Add initial support for msm8998
    Change-Id: I1f3230594e74d32fe0440afbbac4d48d6e0d4516

---
+++ soc/msm8998/product.mk
@@ -0,0 +1,18 @@
+#
+# SPDX-FileCopyrightText: The LineageOS Project
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Firmware
+PRODUCT_PACKAGES += \
+    linux_firmware_qcom-a540
+
+# Graphics (Mesa)
+ifeq ($(TARGET_GRAPHICS),mesa)
+PRODUCT_VENDOR_PROPERTIES += \
+    ro.opengles.version=196610
+endif
+
+# Init
+PRODUCT_PACKAGES += \
+    init.mainline.qcom.msm8998.rc
