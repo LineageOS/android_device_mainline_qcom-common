3304d0f9f101e4efba27ce834ecf02ab0e484425
Yumi Yukimura <me.cafebabe@gmail.com>
2026-08-07 18:43:19 +0800

    mainline/qcom-common: soc: Add initial support for msm8956
    Change-Id: Ic54949edab338fd088fd514fde8aea8ebb1a84f9

---
+++ soc/msm8956/product.mk
@@ -0,0 +1,33 @@
+#
+# SPDX-FileCopyrightText: The LineageOS Project
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Audio
+$(call soong_config_set,mainline_qcom_common_soc,primary_audio_policy_configuration_variant,msm8916)
+
+# Bluetooth
+PRODUCT_VENDOR_PROPERTIES += bluetooth.le.disable_apcf_extended_features?=1
+
+# Firmware
+PRODUCT_PACKAGES += \
+    linux_firmware_qcom-a530
+
+# Firmware symlinks
+PRODUCT_PACKAGES += \
+    mainline_qcom-common_symlink_persist_firmware_qcom_sensors_sns.reg \
+    mainline_qcom-common_symlink_persist_firmware_wlan_prima_WCNSS_qcom_wlan_nv.bin
+
+# Graphics
+PRODUCT_PACKAGES += \
+    msm_drm_quirks
+
+# Graphics (Mesa)
+ifeq ($(TARGET_GRAPHICS),mesa)
+PRODUCT_VENDOR_PROPERTIES += \
+    ro.opengles.version=196609
+endif
+
+# Init
+PRODUCT_PACKAGES += \
+    init.mainline.qcom.msm8956.rc
