851ad37942e93b21182f1b279c3b9f38e053a6d8
Aaron Kling <webgeek1234@gmail.com>
2026-08-11 19:46:27 -0500

    mainline/qcom-common: Enable skiavk by default for mesa
    As of Mesa 26.2, Turnip supports an emulated second graphics queue,
    which is enough for skiavk to operate as expected.
    
    Change-Id: I69613df3226d7c40f156df92f33621fd32a48910

--- optional/mesa/product.mk
+++ optional/mesa/product.mk
@@ -6,6 +6,8 @@
 ifeq ($(TARGET_GRAPHICS),mesa)
 
 ifneq ($(TARGET_QCOM_SOC_FAMILY_IS_LEGACY),true)
+TARGET_USES_VULKAN := true
+
 PRODUCT_VENDOR_PROPERTIES += \
     ro.hardware.vulkan=freedreno
 endif
