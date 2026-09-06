9512a2012546f763dba0bcbaa9b25c369e6418df
Yumi Yukimura <me.cafebabe@gmail.com>
2026-09-06 23:17:17 +0800

    mainline/qcom-common: sensors-hal_mainline: Enable composite device orientation sensor for legacy SoCs
    See https://wiki.postmarketos.org/wiki/Qualcomm_Snapdragon_Sensor_Core
    for the reason to guard this for only legacy SoC families.
    
    So far, I see these legacy SoC platforms are using either `qcom_smgr`
    sensors driver, or bitbang on the GPIO to get sensors I2C bus access,
    and then use normal IIO sensors drivers found in Linux kernel.
    
    Without this, none of these legacy SoC platforms have working
    auto rotation by default, due to Android's complicated algorithms
    in frameworks/base/services/core/java/com/android/server/wm/WindowOrientationListener.java
    
    Auto rotation works by default on devices with SSC sensors, though.
    
    Change-Id: I93054a67e690d373eab79a729578bcb3db9b093c

--- optional/sensors-hal_mainline/product.mk
+++ optional/sensors-hal_mainline/product.mk
@@ -10,6 +10,11 @@ PRODUCT_VENDOR_PROPERTIES += \
     vendor.sensors.wait_for_sensors_interval_ms?=1000 \
     vendor.sensors.wait_for_sensors_timeout_ms?=30000
 
+ifeq ($(TARGET_QCOM_SOC_FAMILY_IS_LEGACY),true)
+PRODUCT_VENDOR_PROPERTIES += \
+    vendor.sensors.composite.device_orientation.enabled?=true
+endif
+
 _is_libssc_available := $(if $(wildcard external/libssc/Android.bp external/mainline-hw-deps/libssc/Android.bp),true,false)
 $(call soong_config_set_bool,libsensors_libssc,enabled,$(_is_libssc_available))
 
