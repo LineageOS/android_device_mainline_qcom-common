b583ac25071889a2932c85c713874c7b3c8039eb
Yumi Yukimura <me.cafebabe@gmail.com>
2026-09-06 23:16:08 +0800

    mainline/qcom-common: sensors-hal_mainline: Wait for at least one sensor
    Change-Id: Iced536abe5e01874b219dd7d4605274b6c22d1a2

--- optional/sensors-hal_mainline/product.mk
+++ optional/sensors-hal_mainline/product.mk
@@ -5,6 +5,9 @@
 
 ifeq ($(TARGET_SENSORS_HAL),mainline)
 
+PRODUCT_VENDOR_PROPERTIES += \
+    vendor.sensors.wait_for_sensors?=1
+
 _is_libssc_available := $(if $(wildcard external/libssc/Android.bp external/mainline-hw-deps/libssc/Android.bp),true,false)
 $(call soong_config_set_bool,libsensors_libssc,enabled,$(_is_libssc_available))
 
